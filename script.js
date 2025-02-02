dacbcfdf1cc9abc27283e3d467f3a5a74d905d59
kartik132000 <sujeetkumarsen37@gmail.com>
2025-02-02 10:45:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function highlight() {
     //Write your code here
+		let boldelement = document.queryselector('b');
+		boldelement.foreach(element =>{
+			element.style.color = 'green';
+		});
+	}
 
-
-}
 
 
 function return_normal() {
     //Write your code here
+	let boldelement = document.queryselectorAll('b');
+	boldelement.foreach(boldelement =>{
+		element.style.color = 'black';
+	});
 
+	
     
 }
