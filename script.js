b04659aac2c6348780ef766c26d02196f96ce4a2
kartik132000 <sujeetkumarsen37@gmail.com>
2025-02-02 10:53:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function highlight() {
-    //Write your code here
-		let boldelement = document.queryselector('b');
-		boldelement.foreach(element =>{
-			element.style.color = 'green';
-		});
-	}
-
+    // Select all bold elements
+    let boldElements = document.querySelectorAll('strong');
+    
+    // Change color to green
+    boldElements.forEach(element => {
+        element.style.color = 'green';
+    });
+}
 
 function return_normal() {
-    //Write your code here
-	let boldelement = document.queryselectorAll('b');
-	boldelement.foreach(boldelement =>{
-		element.style.color = 'black';
-	});
-
-	
+    // Select all bold elements
+    let boldElements = document.querySelectorAll('strong');
     
+    // Change color back to black
+    boldElements.forEach(element => {
+        element.style.color = 'black';
+    });
 }
